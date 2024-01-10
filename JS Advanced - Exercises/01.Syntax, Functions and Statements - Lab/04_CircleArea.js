d776fbaa96dcb41b3d712a42a24e3560cf92939e
Pavlin Vasilev <shadow_psv@yahoo.com>
2024-01-10 19:00:39 +0200

    04_CircleArea.js

---
+++ JS Advanced - Exercises/01.Syntax, Functions and Statements - Lab/04_CircleArea.js
@@ -0,0 +1,11 @@
+function circleArea(input) {
+    let type = typeof (input);
+    if (type == "number") {
+        res = Math.pow(input, 2) * Math.PI;
+        console.log(res.toFixed(2));
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${type}.`)
+    }
+}
+circleArea(5)
+circleArea('name')
