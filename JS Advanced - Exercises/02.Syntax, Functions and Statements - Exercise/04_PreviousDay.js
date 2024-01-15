241f320945112a67d435b47ddc4a70f035e26448
Pavlin Vasilev <shadow_psv@yahoo.com>
2024-01-15 18:15:26 +0200

    04_PreviousDay.js

--- JS Advanced - Exercises/02.Syntax, Functions and Statements - Exercise/04_PreviousDay.js
+++ JS Advanced - Exercises/02.Syntax, Functions and Statements - Exercise/04_PreviousDay.js
@@ -1,5 +1,8 @@
-function prevousDay(year, month, day) {
-    
+function previousDay(year, month, day) {
+    let date = new Date(year, month - 1, day);
+    date.setDate(date.getDate() - 1);
+    let formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    console.log(formattedDate);
 }
-prevousDay(2016, 9, 30)
-prevousDay(2015, 5, 10)
+previousDay(2016, 9, 30)
+previousDay(2015, 5, 10)
